523a6eb81280965ecb3534c0147f1979187d0a85
Cameron Rojas <rscameron7@gmail.com>
2021-04-21 14:02:52 -0700

    built base grid

--- src/components/headerWrapper.js
+++ src/components/headerWrapper.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import HeaderBar from './headerBar';
 
 class headerWrapper extends Component {
     render() {
-        return (
+        return [
             <div className='header-wrapper'>
                 <h1 className='header-wrapper_title'>Welcome to HOA Manager!</h1>
                 <p className='header-wrapper_subtitle'>Please login to continue</p>
-                {this.props.children}
             </div>
-        )
+            HeaderBar(), 
+            this.props.children
+        ]
     }
 }
 
