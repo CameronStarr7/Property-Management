bc98af9f5089c776552bf5adf7f48dcc1c86d89e
Cameron Rojas <rscameron7@gmail.com>
2021-04-25 10:18:07 -0700

    login field component, text-link component, and remaining signin styles

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'react-redux';
 
 import { FormTitle } from '../formTitle';
-import { FormInput } from '../formField';
+import { FormInput, FormButton } from '../formField';
+import {TextLink} from '../textLink';
 
 class SigninForm extends Component {
     render() {
@@ -25,6 +26,17 @@ class SigninForm extends Component {
                     title="Password"
                     component = {FormInput} 
                 />
+                <Field 
+                    className="sign-in-form_login"
+                    name="login"
+                    type="submit"
+                    title="Login"
+                    component = {FormInput} 
+                />
+                <div className='sign-in-form_text-links'>
+                    <TextLink to='/forgot' text='Forgot Password'/>
+                    <TextLink to='/signup' text='Not a member? Register here'/>
+                </div>
             </form>
         );
     }
--- src/components/formField.js
+++ src/components/formField.js
@@ -4,14 +4,29 @@ export class FormInput extends Component {
     render() {
         const { className, title, input, type, placeholder } = this.props;
         return (
-            <div className='form-input'>
-                <label>{title}</label>
+            <div className={`${className} form-input`}>
+                <label className='form-input_title'>{title}</label>
                 <input
-                    className={`${className} form-input`}
+                    className='form-input_input'
                     type={type}
                     {...input}
                     placeholder={placeholder}
                     />
+            </div>
+        )
+    }
+}
+
+export class FormButton extends Component {
+    render() {
+        const { className, title, input, type } = this.props;
+        return (
+            <div className={`${className} form-button`}>
+                <button
+                    className='form-button_button'
+                    type={type}
+                    {...input}
+                ></button>
             </div>
         )
     }
