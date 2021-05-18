fea1680e57fbd89d9c9fbc675137beb7d4c25199
Cameron Rojas <rscameron7@gmail.com>
2021-05-17 22:19:57 -0700

    tab-nav grid

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class TabNav extends Component {
     render() {
         return (
-            <div className='tab-nav'> 
+            <div className='tab-nav'>
+                <div className='tab-nav_items'>
+                {
+                    this.props.tabs.map((tab, index) => {
+                        return <a className='tab-nav_item'>{tab.title}</a>
+                    })
+                }
+                </div>
+                <div>requests or newsletters content goes here</div>
             </div>
         )
     }
