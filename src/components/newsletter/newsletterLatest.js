d734e698e96f8bce41d5658f600283d99c5017ba
Cameron Rojas <rscameron7@gmail.com>
2021-05-21 14:46:02 -0700

    styled newsletter latest and applied row gaps to newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -6,7 +6,7 @@ class NewsletterLatest extends Component {
                 <h1 className='newsletter-latest_title'>Title goes here</h1>
                 <img className='newsletter-latest_image' src='http://via.placeholder.com/960x258'/>
                 <div className='newsletter-latest_body'>
-                    <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since the 1500's when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into the electronic typesetting, remaining essentially unchanged. It was popularized in the 1960's with the release of Letraset sheets containing Lorem ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
             </div>
         )
