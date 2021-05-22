89f792350c37057ca10dd674435a57395eb25c59
Cameron Rojas <rscameron7@gmail.com>
2021-05-22 09:58:17 -0700

    mapped over a newsletter JSON item to the latest newsletter

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -4,12 +4,21 @@ import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
     render() {
+
+        const latest = {
+            _id: '115',
+            title: 'Happy Holidays Fam',
+            body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into the electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            date: new Date(),
+            imageUrl: 'http://via.placeholder.com/968x258'
+        }
+
         return (
             <div className='newsletter-grid'>
             {/* add button */}
             <NewsletterBox date={new Date()}/> 
             <NewsletterArchive/>
-            <NewsletterLatest/>
+            <NewsletterLatest {...latest}/>
             </div>
         )
     }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 class NewsletterLatest extends Component {
     render() {
+        const { title, imageUrl, body }= this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest_title'>Title goes here</h1>
-                <img className='newsletter-latest_image' src='http://via.placeholder.com/960x258'/>
+                <h1 className='newsletter-latest_title'>{title}</h1>
+                <img className='newsletter-latest_image' src={imageUrl}/>
                 <div className='newsletter-latest_body'>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since the 1500's when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into the electronic typesetting, remaining essentially unchanged. It was popularized in the 1960's with the release of Letraset sheets containing Lorem ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p>{body}</p> 
                 </div>
             </div>
         )
     }
 }
+
+export default NewsletterLatest; 
