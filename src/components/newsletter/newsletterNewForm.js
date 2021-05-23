9c87ccbc1745877885964ef395ef988367805e59
Cameron Rojas <rscameron7@gmail.com>
2021-05-23 10:37:27 -0700

    built newsletter new form

---
+++ src/components/newsletter/newsletterNewForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'react-redux';
+
+import { FormTitle } from '../formTitle';
+import { FormInput, FormButton } from '../formField';
+import {TextLink} from '../textLink';
+
+class NewNewsletterForm extends Component {
+    render() {
+
+        const { handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit} className='sign-in-form'>
+                <FormTitle className='sign-in-form_title' text='Login'/>
+                <Field 
+                    className="sign-in-form_email"
+                    placeholder="Email" 
+                    name="email"
+                    type="email"
+                    title="Email"
+                    component = {FormInput} 
+                />
+                <Field 
+                    className="sign-in-form_password"
+                    placeholder="Enter Password" 
+                    name="password"
+                    type="password"
+                    title="Password"
+                    component = {FormInput} 
+                />
+                <Field 
+                    className="sign-in-form_login"
+                    name="login"
+                    type="submit"
+                    title="Login"
+                    component = {FormInput} 
+                />
+                <div className='sign-in-form_text-links'>
+                    <TextLink to='/forgot' text='Forgot Password'/>
+                    <TextLink to='/signup' text='Not a member? Register here'/>
+                </div>
+            </form>
+        );
+    }
+}
+
+NewNewsletterForm = reduxForm({
+    form: "newnewsletter"
+ }) (NewNewsletterForm);
+ 
+export default NewNewsletterForm;
