eeb8171744efb381ff7e3b09d5e6838170daab4f
Cameron Rojas <rscameron7@gmail.com>
2021-05-28 14:46:05 -0700

    added component to edit newsletters and updated bootstrap. commit early

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -9,14 +9,18 @@ import reducers from "./reducers";
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import '../src/style/main';
-
+import history from './history';
 import Layout from './components/layout';
 
+//AUTH
+import requireAuth from "./components/requireAuth";
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
-import history from './history';
-import requireAuth from "./components/requireAuth";
+
+//DASHBOARD
 import Dashboard from './components/dashboard';
+import NewNewsletter from './components/newsletter/newsletterNew';
+import EditNewsletter from './components/newsletter/newsletterEdit';
 
 function main() {
   ReactDOM.render(
@@ -29,7 +33,8 @@ function main() {
             <Route path='/signup' component={Signup}/>
 
             <Route path='/dashboard' component={requireAuth(Dashboard)}/>
-            <Route path='/newsletter/new' component={requireAuth(NewsNewsletter)}/>
+            <Route path='/newsletter/New' component={requireAuth(NewNewsletter)}/>
+            <Route path='/newsletter/Edit' component={requireAuth(EditNewsletter)}/>
           </Layout>
         </Switch>
       </Router>
---
+++ src/components/newsletter/newsletterEdit.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import NewNewsletterForm from './newsletterNewForm';
+
+class EditNewsletter extends Component {  
+
+    onSubmit = (fields) => {
+        // if(button == 'submit') {
+        //     //save new newsletter on the backend. perform a post request here.
+        //     console.log('trying to handle submit on backend.');
+        // }
+        this.props.history.push('/dashboard');
+    }
+
+    onCancel = () => {
+        // this.props.history.push('/dashboard')
+        console.log('trying to cancel');
+    }
+
+    render() { 
+        return (
+            <div className='new-newsletter'>
+                <NewNewsletterForm onCancel={() => this.onCancel()} onSubmit={(event) => this.onSubmit(event)}/>
+            </div>
+        )
+    } 
+}
+
+export default NewNewsletter;
