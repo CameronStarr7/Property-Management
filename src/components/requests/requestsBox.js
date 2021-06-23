360f6ffb07a747592aba1841ed26d416601ddec1
Cameron Rojas <rscameron7@gmail.com>
2021-06-22 22:27:36 -0700

    built requests reducer and change selected request type action creator

--- src/components/requests/requestsBox.js
+++ src/components/requests/requestsBox.js
@@ -4,7 +4,7 @@ class RequestsBox extends Component {
     render() {
         const { count, title } = this.props;
         return (
-            <a onClick={() => console.log('tryna select this box')} className='requests-box requests-box-inactive'>
+            <a onClick={(title) => this.props.changeSelectedRequestType(title)} className='requests-box requests-box-inactive'>
                 <div className='requests-box_count'>{count}</div>
                 <div className='requests-box_title'>{title}</div>
                 <div className='requests-box_point'></div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
+
 import{ reducer as form } from 'redux-form';
+
 import auth from './authReducer';
 import newsletterReducer from './newsletterReducer';
+import requestsReducer from './requestsReducer';
 
 const rootReducer = combineReducers({
   form,
   auth,
-  newsletterReducer
+  newsletters,
+  requests
 });
 
 export default rootReducer;
