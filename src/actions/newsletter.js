6dcfc67deeea873cf5fb3cfa5929bea4fd6ed313
Cameron Rojas <rscameron7@gmail.com>
2021-08-02 12:20:33 -0700

    refactored fetchnewsletters to actually fetch from backend

--- src/actions/newsletter.js
+++ src/actions/newsletter.js
@@ -3,28 +3,24 @@ import {
     FETCH_NEWSLETTER_ID
 } from './types'; 
 
+import axios from 'axios';
+import { ROOT_URL } from '../config';
+
 export function fetchNewsletters() {
-    const response = {
-        data: [
-            {
-                _id: '115',
-                title: 'Happy Holidays Fam',
-                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into the electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                date: new Date(),
-                imageUrl: 'http://via.placeholder.com/968x258'
-            },
-            {
-                _id: '115',
-                title: 'Happy Holidays Fam',
-                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into the electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                date: new Date(),
-                imageUrl: 'http://via.placeholder.com/968x258'
-            }
-        ]
-    }
-    return {
-        type: SET_NEWSLETTERS,
-        payload: response.data
+
+    return function (dispatch) {
+        const token = localStorage.getItem('token');
+        const headers = {headers: {authorization: token}}
+        axios.get(`${ROOT_URL}/newsletters`, headers)
+            .then(response => {
+                dispatch({
+                    type: SET_NEWSLETTERS,
+                    payload: response.data
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 }
 
