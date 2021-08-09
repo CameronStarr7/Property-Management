3fb6dcead09ade8218ba9a171a7a14156619515e
Cameron Rojas <rscameron7@gmail.com>
2021-08-09 09:41:01 -0700

    provided props to header title subtitle

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 
 export function Header() {
+        
         return (
                 <div className='header'>
-                    <h1 className='header_title'>Welcome to HOA Manager!</h1>
-                    <p className='header_subtitle'>Please login to continue</p>
+                    <h1 className='header_title'>{title}</h1>
+                    <p className='header_subtitle'>{subtitle}</p>
                 </div>
         )
 }
